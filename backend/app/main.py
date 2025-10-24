3de92768b59757eac91fbf0457a3486401517487
Bartłomiej Rasztabiga <contact@rasztabiga.me>
2025-10-24 22:57:09 +0200

    Remove reverse proxy

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Konfiguracja CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://frontend:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
